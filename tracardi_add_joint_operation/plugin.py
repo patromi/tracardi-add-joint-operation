a3c78b30197d970bf3e38675aa9053bd6eaea497
admin <patromi123@gmail.com>
2021-10-31 23:13:04 +0100

    initial commit

--- tracardi_add_joint_operation/plugin.py
+++ tracardi_add_joint_operation/plugin.py
@@ -1,7 +1,7 @@
 from tracardi_plugin_sdk.action_runner import ActionRunner
-from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData
 from tracardi_plugin_sdk.domain.result import Result
 from tracardi_add_joint_operation.model.models import Module
+from tracardi_plugin_sdk.domain.register import Plugin, Spec, MetaData, Form, FormGroup, FormField, FormComponent
 
 
 def join(array, string):
@@ -26,11 +26,34 @@ def register() -> Plugin:
             module='tracardi_add_joint_operation.plugin',
             className='JoinAction',
             inputs=["payload"],
-            outputs=['payload'],
-            version='0.1',
+            outputs=["payload"],
+            init={
+                'array': None,
+                'join_string': None
+            },
+            form=Form(groups=[
+                FormGroup(
+                    fields=[
+                        FormField(
+                            id="array",
+                            name="array",
+                            description="Choose your array from dot notation to string conversion",
+                            component=FormComponent(type="text", props={"label": "array"})
+                        ),
+                        FormField(
+                            id="join_string",
+                            name="Join string",
+                            description="Type string to join action.",
+                            component=FormComponent(type="text", props={"label": "event"})
+                        )
+                    ]
+                ),
+            ]),
+            manual="regex_validator_action",
+            version='0.6.1',
             license="MIT",
-            author="Patryk Migaj",
-            init={"join_string": "None"}
+            author="Patryk Migaj"
+
         ),
         metadata=MetaData(
             name='tracardi-add-joint-operation',
